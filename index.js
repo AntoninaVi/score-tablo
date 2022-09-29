968fc6e7ec2892a644900fc59f5b463d7b8bad24
AntoninaVi <tonatonotonya@gmail.com>
2022-09-29 23:38:28 +0300

    scoreboard

---
+++ index.js
@@ -0,0 +1,59 @@
+let num1 = 0
+
+let sumEL = document.getElementById("sum")
+
+function add1() {
+    let result = num1 + 1
+    sumEL.textContent = + result
+    num1 = num1 + 1
+}
+console.log(num1)
+
+function add2() {
+    let result = num1 + 2
+    sumEL.textContent = + result
+    num1 = num1 + 2
+}
+console.log(num1)
+
+function add3() {
+    let result = num1 + 3
+    sumEL.textContent = + result
+    num1 = num1 + 3
+}
+console.log(num1)
+
+
+let num2 = 0
+
+let guest = document.getElementById("guest")
+
+
+function addone() {
+    let result = num2 + 1
+    guest.textContent = + result
+    num2 = num2 + 1
+}
+console.log(num2)
+
+function addtwo() {
+    let result = num2 + 2
+    guest.textContent = + result
+    num2 = num2 + 2
+}
+console.log(num2)
+
+function addthree() {
+    let result = num2 + 3
+    guest.textContent = + result
+    num2 = num2 + 3
+}
+console.log(num2)
+
+function reset(){
+    sumEL = 0
+    guest = 0
+    sumEL.textContent = num1
+    guest.textContent = num2
+
+}
